8a6e3adfe836f7ad4db0ca9ebb461bfb3962de4b
jdavasligil <22987230+jdavasligil@users.noreply.github.com>
2024-05-09 11:00:51 -0700

    One shot layer handles logic / keybinds while layer 2 does punctuation

--- keyboards/ferris/keymaps/jdavasligil/keymap.c
+++ keyboards/ferris/keymaps/jdavasligil/keymap.c
@@ -8,24 +8,29 @@
  */
 
 enum custom_keycodes {
-    CS_LARROW = SAFE_RANGE,
-    CS_RARROW,
+    CS_LARRW = SAFE_RANGE,
+    CS_RARRW,
     CS_LEQ,
     CS_GEQ,
     CS_EQ,
     CS_NEQ,
-    CS_WALRUS,
+    CS_AND,
+    CS_OR,
+    CS_LSFT,
+    CS_RSFT,
+    CS_WLRS,
+    CS_THEN,
     CS_ERR,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-    case CS_LARROW:
+    case CS_LARRW:
         if (record->event.pressed) {
             SEND_STRING("<-");
         }
         break;
-    case CS_RARROW:
+    case CS_RARRW:
         if (record->event.pressed) {
             SEND_STRING("->");
         }
@@ -50,9 +55,34 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             SEND_STRING("!=");
         }
         break;
-    case CS_WALRUS:
+    case CS_AND:
+        if (record->event.pressed) {
+            SEND_STRING("&&");
+        }
+        break;
+    case CS_OR:
+        if (record->event.pressed) {
+            SEND_STRING("||");
+        }
+        break;
+    case CS_LSFT:
+        if (record->event.pressed) {
+            SEND_STRING("<<");
+        }
+        break;
+    case CS_RSFT:
+        if (record->event.pressed) {
+            SEND_STRING(">>");
+        }
+        break;
+    case CS_WLRS:
         if (record->event.pressed) {
             SEND_STRING(":=");
+        }
+        break;
+    case CS_THEN:
+        if (record->event.pressed) {
+            SEND_STRING("=>");
         }
         break;
     case CS_ERR:
@@ -65,11 +95,31 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_split_3x5_2(KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, LSFT_T(KC_A), KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LSFT_T(KC_SCLN), LCTL_T(KC_Z), LALT_T(KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, RALT_T(KC_DOT), RCTL_T(KC_SLSH), KC_LGUI, LT(3,KC_BSPC), LT(2,KC_SPC), OSL(1)),
-	[1] = LAYOUT_split_3x5_2(KC_TILDE, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_BSLS, KC_TAB, KC_GRV, KC_LPRN, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_RPRN, KC_QUOT, KC_ENT, CS_ERR, CS_NEQ, CS_LEQ, CS_LARROW, KC_LT, KC_GT, CS_RARROW, CS_GEQ, CS_EQ, CS_WALRUS, KC_DEL, KC_ESC, KC_INS, TO(4)),
-	[2] = LAYOUT_split_3x5_2(KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, LSFT_T(KC_1), KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, RSFT_T(KC_0), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LSFT, KC_BSPC, KC_NO, KC_NO),
-	[3] = LAYOUT_split_3x5_2(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_DEL, KC_P7, KC_P8, KC_P9, KC_PEQL, KC_TAB, KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS, KC_BSPC, KC_P4, KC_P5, KC_P6, KC_PENT, KC_TRNS, KC_TRNS, KC_KP_DOT, KC_UNDERSCORE, KC_TRNS, KC_P0, KC_P1, KC_P2, KC_P3, KC_PCMM, KC_NO, KC_NO, KC_SPC, KC_TRNS),
-	[4] = LAYOUT_split_3x5_2(KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, KC_BTN2, KC_LSFT, KC_END, KC_PGDN, KC_PGUP, KC_HOME, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_RSFT, KC_LCTL, KC_LALT, KC_BRID, KC_BRIU, RGB_TOG, KC_MUTE, KC_VOLU, KC_VOLD, KC_RALT, KC_RCTL, KC_LGUI, LT(3,KC_BSPC), KC_MPLY, TG(4))
+	[0] = LAYOUT_split_3x5_2(
+            KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
+            LSFT_T(KC_A), KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LSFT_T(KC_SCLN),
+            LCTL_T(KC_Z), LALT_T(KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, RALT_T(KC_DOT), RCTL_T(KC_SLSH),
+            KC_LGUI, LT(3,KC_BSPC), LT(2,KC_SPC), OSL(1)),
+	[1] = LAYOUT_split_3x5_2(
+            KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
+            KC_EXLM, CS_AND, CS_NEQ, CS_LEQ, KC_LT, KC_GT, CS_GEQ, CS_EQ, CS_OR, KC_EQL,
+            CS_THEN, KC_AMPR, KC_TILDE, CS_LARRW, CS_LSFT, CS_RSFT, CS_RARRW, KC_CIRC, KC_PIPE, CS_WLRS,
+            CS_ERR, KC_TRNS, KC_TRNS, TO(4)),
+	[2] = LAYOUT_split_3x5_2(
+            KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
+            LSFT_T(KC_TAB), KC_GRV, KC_LPRN, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_RPRN, KC_QUOT, RSFT_T(KC_ENT),
+            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+            KC_NO, KC_BSPC, KC_NO, KC_NO),
+	[3] = LAYOUT_split_3x5_2(
+            KC_TILDE, KC_PERC, KC_CIRC, KC_EXLM, KC_INS, KC_DEL, KC_P7, KC_P8, KC_P9, KC_PEQL,
+            KC_ESC, KC_PSLS, KC_PAST, KC_PMNS, KC_PPLS, KC_BSPC, KC_P4, KC_P5, KC_P6, KC_PENT,
+            KC_HASH, KC_BSLS, KC_KP_DOT, KC_UNDS, KC_EQL, KC_P0, KC_P1, KC_P2, KC_P3, KC_PCMM,
+            KC_NO, KC_NO, KC_SPC, OSL(1)),
+	[4] = LAYOUT_split_3x5_2(
+            KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, KC_BTN2,
+            KC_LSFT, KC_END, KC_PGDN, KC_PGUP, KC_HOME, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_RSFT,
+            KC_LCTL, KC_LALT, KC_BRID, KC_BRIU, RGB_TOG, KC_MUTE, KC_VOLU, KC_VOLD, KC_RALT, KC_RCTL,
+            KC_LGUI, LT(3,KC_BSPC), KC_MPLY, TG(4))
 };
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
