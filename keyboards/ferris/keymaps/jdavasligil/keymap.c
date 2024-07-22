c01b580012f24b94e639ac54f0e7e76d96757050
jdavasligil <22987230+jdavasligil@users.noreply.github.com>
2024-07-22 11:56:58 -0700

    Fix caps word

--- keyboards/ferris/keymaps/jdavasligil/keymap.c
+++ keyboards/ferris/keymaps/jdavasligil/keymap.c
@@ -97,7 +97,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_split_3x5_2(
             KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
-            LSFT_T(KC_A), KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LSFT_T(KC_SCLN),
+            LSFT_T(KC_A), KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, RSFT_T(KC_SCLN),
             LCTL_T(KC_Z), LALT_T(KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, RALT_T(KC_DOT), RCTL_T(KC_SLSH),
             KC_LGUI, LT(3,KC_BSPC), LT(2,KC_SPC), OSL(1)),
 	[1] = LAYOUT_split_3x5_2(
